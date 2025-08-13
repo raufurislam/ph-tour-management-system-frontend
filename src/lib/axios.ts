34c3a259a9c148ba0be879442b229d437cf8668d
Raufur Islam <raufurislam@gmail.com>
2025-08-13 16:48:06 +0600

    36-11 Visualizing axiosBaseQuery Payload and Router State

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -9,6 +9,7 @@ export const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
   function (config) {
     // Do something before request is sent
+    console.log("Axios", config);
     return config;
   },
   function (error) {
@@ -22,6 +23,7 @@ axiosInstance.interceptors.response.use(
   function onFulfilled(response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
+    console.log("Axios", response);
     return response;
   },
   function onRejected(error) {
