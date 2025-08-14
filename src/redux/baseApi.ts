f2062ce92fc5685b6312eff3d3727aba2dab8057
Raufur Islam <raufurislam@gmail.com>
2025-08-14 14:30:13 +0600

    37-7 Accessing Cookies on the Client Side

--- src/redux/baseApi.ts
+++ src/redux/baseApi.ts
@@ -4,5 +4,9 @@ import axiosBaseQuery from "./axiosBaseQuery";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: axiosBaseQuery(),
+  // baseQuery: fetchBaseQuery({
+  //   baseUrl: config.baseUrl,
+  //   credentials: "include",
+  // }),
   endpoints: () => ({}),
 });
