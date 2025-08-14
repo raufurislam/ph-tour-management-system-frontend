24eac32d388f93b6ed68de485d25c859189ec553
Raufur Islam <raufurislam@gmail.com>
2025-08-14 21:44:27 +0600

    38-1 Setting Up the Dashboard and Admin Routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,11 @@
 import App from "@/App";
+import DashboardLayout from "@/components/layout/DashboardLayout";
 import About from "@/pages/About";
+import AddTour from "@/pages/Admin/AddTour";
+import Analytics from "@/pages/Admin/Analytics";
 import Login from "@/pages/Login";
 import Register from "@/pages/Register";
+import Bookings from "@/pages/User/Bookings";
 import Verify from "@/pages/Verify";
 import { createBrowserRouter } from "react-router";
 
@@ -13,6 +17,30 @@ export const router = createBrowserRouter([
       {
         Component: About,
         path: "/about",
+      },
+    ],
+  },
+  {
+    Component: DashboardLayout,
+    path: "/admin",
+    children: [
+      {
+        Component: Analytics,
+        path: "analytics",
+      },
+      {
+        Component: AddTour,
+        path: "add-tour",
+      },
+    ],
+  },
+  {
+    Component: DashboardLayout,
+    path: "/user",
+    children: [
+      {
+        Component: Bookings,
+        path: "bookings",
       },
     ],
   },
