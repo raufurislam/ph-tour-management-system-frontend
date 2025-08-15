52f10069470f73c1ab61f0fb4c5655bdcf79e998
Raufur Islam <raufurislam@gmail.com>
2025-08-16 02:34:20 +0600

    Single file upload changes

--- src/components/SingleImageUploader.tsx
+++ src/components/SingleImageUploader.tsx
@@ -3,7 +3,8 @@ import { AlertCircleIcon, ImageUpIcon, XIcon } from "lucide-react";
 import { useFileUpload } from "@/hooks/use-file-upload";
 import { useEffect } from "react";
 
-export default function SingleImageUploader({ onChange }) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function SingleImageUploader({ onChange }: any) {
   const maxSizeMB = 5;
   const maxSize = maxSizeMB * 1024 * 1024; // 5MB default
 
